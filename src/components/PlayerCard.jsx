df3435d992d39e2cca3125fdfd21e08d0ffe53b9
mrlmhl <meralorguc1@gmail.com>
2023-12-15 12:18:46 +0300

    first commit

---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+
+const PlayerCard =({img, name, statistics})=>{
+  const [showImage, setShowImage] = useState(true)
+  const handleClick =()=>setShowImage(!showImage)
+
+    return(
+        <Card className='rounded-2 m-auto player-card' role="button" onClick={handleClick}  >
+          {showImage ? (
+          <Card.Img className='player-logo' variant="top" src={img} /> 
+          ) : (
+          <ul className='m-auto'>
+            {statistics.map((item, i )=>(
+              <li className='list-unstyled h5 text-start' key={i}>🏀{item}</li>
+            ))}
+          </ul>
+          )}
+          <Card.Footer>
+            <Card.Title>{name}</Card.Title>
+          </Card.Footer>
+        </Card>
+    );
+
+};
+
+export default PlayerCard;
